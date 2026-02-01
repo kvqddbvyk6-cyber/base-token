d4c39c2aa1fae8c0ad91936edca2b8e26aea5916
Elmz <elmz@Mac.lan>
2026-02-01 03:18:03 -0800

    cursor: empty commit to allow worktree creation

---
+++ script/cp_amm/load-keypair.ts
@@ -0,0 +1,24 @@
+import { Keypair } from "@solana/web3.js";
+import * as fs from "fs";
+
+/**
+ * Load a Solana keypair from env:
+ * - PRIVATE_KEY: base58 string or JSON array of bytes
+ * - KEYPAIR_PATH: path to a JSON keypair file
+ */
+export function loadKeypair(): Keypair {
+  const secret = process.env.PRIVATE_KEY;
+  const keypairPath = process.env.KEYPAIR_PATH;
+  if (!secret && !keypairPath) {
+    throw new Error( "Set PRIVATE_KEY (base58 or JSON array) or KEYPAIR_PATH");
+  }
+  if (secret) {
+    if (secret.startsWith("[")) {
+      return Keypair.fromSecretKey(Uint8Array.from(JSON.parse(secret)));
+    }
+    const bs58 = require("bs58");
+    return Keypair.fromSecretKey(bs58.decode(secret));
+  }
+  const data = JSON.parse(fs.readFileSync(keypairPath!, "utf-8"));
+  return Keypair.fromSecretKey(Uint8Array.from(data));
+}
